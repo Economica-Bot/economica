8192731b30e129608bbb0386e6f34ff02f833798
Gabriel Hall <gabriel.hall@cox.net>
2021-03-31 15:41:42 -0700

    Added "help" command, and general cleanup

--- commands/eval.js
+++ commands/eval.js
@@ -6,18 +6,15 @@ module.exports = {
      exUse: `console.log('Hello World')`,
      callback: (message, arguments, text) => {
           const clean = text => {
-               if (typeof (text) === "string") {
-                    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-               }else return text;
+               if (typeof (text) === "string") return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+               else return text;
           }
 
           const { channel } = message
           if (channel.id === '796906162548637780') {
                try {
                     let _message = eval(`${text}`)
-                    if (typeof _message !== 'string') {
-                         _message = require('util').inspect(_message)
-                    }
+                    if (typeof _message !== 'string') _message = require('util').inspect(_message);
                     if (_message.length + text.length < 2015) {
                          message.channel.send({
                               embed: {
@@ -43,9 +40,7 @@ module.exports = {
                          }
                     })
                }
-          } else {
-               message.author.send('due to security risks with this command, it\'s usage is limited to <#796906162548637780>')
-          }
+          } else message.author.send('Due to security risks with `.eval`, its usage is limited to <#796906162548637780>');
    ***REMOVED***
      _auth: 'BOT:OPERATOR',
      silent: true
