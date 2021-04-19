a0d15384ffbc568871e2260371d8eda640b7b60f
Gabriel Hall <gabriel.hall@cox.net>
2021-04-18 18:10:40 -0700

    Revamp - now returns embed with all permissible commands

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -13,11 +13,11 @@ module.exports = {
         const commands = loadCommands()
 
         //Single-Command Help
-        if(arguments[0]) {
-            for(const command of commands) {
-                for(const name of command.commands) {
-                    if(arguments[0] == name) {
-                        return message.channel.send({
+        if (text) {
+            for (const command of commands) {
+                for (const name of command.commands) {
+                    if (arguments[0] == name) {
+                        return message.channel.send ({
                             embed: {
                                 title: `${name}`,
                                 color: 'GREEN',
@@ -39,50 +39,41 @@ module.exports = {
         let helpEmbed= new Discord.MessageEmbed()
         helpEmbed
         .setTitle("Commands")
-        .setFooter("Bugs? Too bad.")
+        .setFooter("See the commands you have access to")
         .setColor(15277667)
         .setThumbnail("https://cdn.discordapp.com/avatars/796906750569611294/34ba71dfc581a2662ec9ac250860b785.png?size=1024")
-        .setTimestamp();
-
-        for(const command of commands) {
-            //Check perms
+        .setTimestamp()
+        
+        let commandlist = ''
+        for (const command of commands) {
+            //add all permissible commands
             let permissions = command.permission
 
-            if(permissions) {
+            if (permissions) {
                 let hasPermission = true
-                if(typeof permissions === 'string') {
+                if (typeof permissions === 'string') {
                     permissions = [permissions]
                 }
 
                 for(const permission of permissions) {
-                    if(!message.member.hasPermission(permission)) {
+                    if (!message.member.hasPermission(permission)) {
                         hasPermission = false
                         break
                     }
                 }
-
-                if(!hasPermission) {
+                
+                //if the user does not have permission, skip to the next iteration
+                if (!hasPermission) {
+                    //temp
+                    message.channel.send(`Missing perm \`${permissions}\` for command \`${prefix}${command.commands}\``)
                     continue
                 }
             }
-            
-            const mainCommand = 
-                typeof command.commands === 'string'
-                    ? command.commands
-                    : command.commands[0]
-            const args = command.expectedArgs
-                ? ` ${command.expectedArgs}` : ''
-            const {description} = command
-
-            helpEmbed.addFields (
-                {
-                   name:`${mainCommand}`,
-                   value:`Expected Arguments: ${args}\nDescription: ${description}`
-                } 
-            )
+            commandlist = `${commandlist} \`${prefix}${command.commands[0]}\``
         }
 
-        message.channel.send(helpEmbed)
-        
+        helpEmbed.setDescription(`${commandlist}`)
+
+        message.channel.send(helpEmbed)  
   ***REMOVED***
 }
