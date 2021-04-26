f6b0522489d220675f8efa9c343e410bf695decc
Gabriel Hall <gabriel.hall@cox.net>
2021-04-25 19:44:45 -0700

    First commit

---
+++ commands/moderation/kick.js
@@ -0,0 +1,28 @@
+module.exports = {
+    aliases: ['kick'],
+    description: 'Kicks a user',
+    expectedArgs: '<user> <reason>',
+    exUse: 'kamaji',
+    minArgs: 1,
+    maxArgs: 10, 
+    permissions: 'KICK_MEMBERS',
+    callback: (message, arguments, text) => {
+
+        const {member, mentions } = message
+        const target = mentions.users.first();
+        const tag = `<@${member.id}>`
+        if(target) {
+            const targetMember = message.guild.members.cache.get(target.id)
+            targetMember.kick()
+
+            if(text.split(" ").splice(1).join(' '))
+                message.channel.send(`Kicked ${target.tag} for \`${text.split(" ").splice(1).join(' ')}\``)
+            else 
+                message.channel.send(`Kicked ${target.tag}`)
+
+        } else {
+            message.channel.send(`${tag}, Please specify someone to kick`)
+        }
+
+  ***REMOVED***
+}
