3695aaa151ffc2db1be4b3cf11ad329f622bc2a1
Gabriel Hall <gabriel.hall@cox.net>
2021-04-25 19:45:08 -0700

    First commit

---
+++ schemas/mute-sch.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const reqString = {
+    type: String, 
+    required: true
+}
+
+const muteSchema = mongoose.Schema({
+        userID: reqString,
+        guildID: reqString,
+        reason: reqString,
+        staffID: reqString,
+        staffTag: reqString,
+        expires: {
+            type: Date,
+            required: true
+      ***REMOVED***
+        current: {
+            type: Boolean, 
+            required: true,
+      ***REMOVED***
+  ***REMOVED*** 
+    {
+    timestamps: true
+    }
+)
+
+module.exports = mongoose.model('mutes', muteSchema)
