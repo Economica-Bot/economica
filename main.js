af305f633b6344fbb4c882fe166b2e5618a1c2de
QiNG-agar <axbigot@gmail.com>
2021-04-25 17:04:14 -0700

    fetchAllMembers on startup

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client({ fetchAllMembers: true })
 
 const config = require('./config.json')
 const mongo = require('./mongo')
