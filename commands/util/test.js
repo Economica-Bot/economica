bffeb8ebb7017d238977e9fd2f33855220afb102
Gabriel Hall <gabriel.hall@cox.net>
2021-04-28 23:24:01 -0700

    indev

--- commands/util/test.js
+++ commands/util/test.js
@@ -4,13 +4,18 @@ module.exports = {
      aliases: ['test'],
      expectedArgs: '<cmd>',
      minArgs: 1,
-     maxArgs: 1, 
+     maxArgs: 10, 
      permissions: ['ADMINISTRATOR'],
      _auth: 'BOT:OPERATOR',
      exUse: 'bal',
      description: 'test',
      callback: (message, arguments, text) => {
           let _message = ''
+          if(arguments[0] === 'omitprefix') {
+               arguments.forEach(argument => _message = `${_message}${argument} `)
+               _message = _message.substr(_message.indexOf(" ") + 1);
+               message.channel.send(`${_message}`)
+          }
           arguments.forEach(argument => _message = `${_message}${argument} `)
 
           message.channel.send(`${prefix}${_message}`)
