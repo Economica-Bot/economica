f82b214b00ad13b954e1ec58408619a1b7c4c320
QiNG-agar <axbigot@gmail.com>
2021-05-12 21:57:57 -0700

    getCurrencySymbol and setCurrencySymbol

--- schemas/prefix-sch.js
+++ schemas/prefix-sch.js
@@ -5,9 +5,13 @@ const prefixSchema = mongoose.Schema(
      {
           _id: {
                type: String,
-               required: false
+               required: true
           },
           prefix: {
+               type: String,
+               required: false
+          },
+          currency: {
                type: String,
                required: false
           }
