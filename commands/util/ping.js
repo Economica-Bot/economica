fdbed92a57749278c4eb1228e8b2e8774419c7a7
QiNG-agar <axbigot@gmail.com>
2021-05-15 00:44:22 -0700

    deleted commands folder

--- commands/util/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-     aliases: ['ping', 'latency'],
-     description: 'Returns the latency in MS',
-     callback: (message, arguments, text) => {
-          const user = message.author
-          message.channel.send(`:ping_pong: **${Date.now() - message.createdTimestamp}**ms <@!${user.id}>`)
-     },
-}
--- commands/util/prefix.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-     aliases: ['prefix', 'getprefix'],
-     description: 'Changes the server prefix to the first argument or returns the current prefix if left blank',
-     callback: (message, arguments, text) => {
-          
-     },
-}
