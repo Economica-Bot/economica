7f8a9b6dce3dada03ce88b70b439aae4388d59a8
Gabriel Hall <gabriel.hall@cox.net>
2021-05-15 00:03:27 -0700

    Moved member choice embed to helper

--- commando-commands/economy/bal.js
+++ commando-commands/economy/bal.js
@@ -22,7 +22,7 @@ module.exports = class BalanceCommand extends Command {
             args: [
                 {
                     key: 'member',
-                    prompt: 'please enter a user name.',
+                    prompt: 'please @mention, name, or provide the id of a user.',
                     type: 'string',
                     default: 'self'
                 }
@@ -32,37 +32,20 @@ module.exports = class BalanceCommand extends Command {
     
     async run(message, { member }) {
         const { guild, author } = message;
-        let user = member == 'self' ? message.author : message.mentions.users.first() || helper.getMemberUserById(message, member) || helper.getMemberUserIdByMatch(message, member);
+        let user = member == 'self' ? author : message.mentions.users.first() || helper.getMemberUserById(message, member) || helper.getMemberUserIdByMatch(message, member);
 
         if (user === 'endProcess') return;
 
-        if (!Array.isArray(user)) return helper.displayBal(message, guild, user);
         if (user.length === 1) return helper.displayBal(message, guild, helper.getMemberUserById(message, user[0]));
 
-        let userList = ``
-        user.forEach(u => {
-            userList = `${userList}\`${user.indexOf(u) + 1}\` - <@!${u}>\n`
-        })
-        const description = `\`${user.length}\` users found. Please type a specific user's key below:\n\n${userList}`
-        
-        message.channel.send({ embed: {
-            author: {
-                name: message.author.tag,
-                icon_url: message.author.avatarURL()
-            },
-            color: 'BLUE',
-            description,
-            footer: {
-                text: 'Type the number corresponding with the user you wish to select. \nNot the users you\'re looking for? Try an @mention when using .bal'
-            }
-        }})
+        helper.createUserEmbedSelection(message, helper.getMemberUserIdByMatch(message, member))
 
-        const collector = helper.createNumberCollector(message, user.length, 30000)
+        const collector = helper.createNumberCollector(message, user.length, 10000)
         collector.on('collect', m => {
             user = helper.getMemberUserById(message, user[parseInt(m.content) - 1])
         })
         collector.on('end', c => {
-            if (c.size === 0) return message.channel.send(':hourglass: Time ran out (30s).')
+            if (c.size === 0) return message.channel.send(':hourglass: Time ran out (10s).')
             helper.displayBal(message, guild, user)
         })
     }
