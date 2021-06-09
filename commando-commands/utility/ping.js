55577d4c6bc4d041476f113fbef34d2c1d8603a6
Gabriel Hall <gabriel.hall@cox.net>
2021-06-08 22:59:38 -0700

    Implement custom ping command

---
+++ commando-commands/utility/ping.js
@@ -0,0 +1,17 @@
+const { Command } = require('discord.js-commando')
+
+module.exports = class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            group: 'util',
+            memberName: 'ping',
+            description: "Returns the latency of the bot's connection.",
+            format: 'ping'
+        })
+    }
+
+    async run(message) {
+        message.channel.send(`Pong! \`${new Date().getTime() - message.createdTimestamp}ms\``)
+    }
+}
