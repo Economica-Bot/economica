cd79ffaa03c0595017dc5fa5ab8c40812edef414
Adrastopoulos <gabriel.hall@cox.net>
2021-07-07 21:25:34 -0700

    Delete helper.js

--- features/util.js
+++ features/util.js
@@ -421,7 +421,7 @@ module.exports.cut = (str, maxLength = 32) => {
  * Changes a user's balance.
  * @param {string} guildID - Guild id.
  * @param {string} userID - User id.
- * @param {number} balance - The value to be added to the user's balance.
+ * @param {Number} balance - The value to be added to the user's balance.
  */
 module.exports.changeBal = async (guildID, userID, balance) => {
     return await mongo().then(async (mongoose) => {
@@ -555,7 +555,7 @@ module.exports.getCurrencySymbol = async (guildID) => {
     const cached = currencyCache[guildID]
     if (cached) {
         return cached
-    } else await mongo().then(async (mongoose) => {
+    } else return await mongo().then(async (mongoose) => {
         try {
             const result = await guildSettingSchema.findOne({
                 _id: guildID,
@@ -569,14 +569,13 @@ module.exports.getCurrencySymbol = async (guildID) => {
             }
 
             currencyCache[guildID] = currency
+            return currencyCache[guildID]
         } catch (err) {
             console.error(err)
         } finally {
             mongoose.connection.close()
         }
     })
-
-    return currencyCache[guildID]
 }
 
 /**
