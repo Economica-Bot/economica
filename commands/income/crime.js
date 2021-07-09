e155181fda6a08bd9d0fbf3e1bdfa6582de0c779
QiNG-agar <axbigot@gmail.com>
2021-07-08 21:33:06 -0700

    refined crime and fixed mongo issue

--- commands/income/crime.js
+++ commands/income/crime.js
@@ -2,7 +2,7 @@ const { Command } = require('discord.js-commando')
 const util = require('../../features/util')
 const ms = require('ms')
 
-module.exports = class BegCommand extends Command {
+module.exports = class CrimeCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'crime',
@@ -20,34 +20,34 @@ module.exports = class BegCommand extends Command {
     }
 
     async run(message) {
-        const { guild, author } = message
-        const currencySymbol = await util.getCurrencySymbol(message.guild.id)
-
-        const properties = await util.getCommandStats(message.guild.id, 'crime')
-        const uProperties = await util.getUserCommandStats(guild.id, author.id, 'crime')
-
-        const now = new Date
-        const usedWhen = now.getTime()
-
-        console.log('crime', properties)
-
-        if ((usedWhen - uProperties.timestamp) < properties.cooldown) {
-            return util.errorEmbed(message, `:hourglass: You need to wait ${ms(properties.cooldown - (Date.now() - uProperties.timestamp))}`, this.memberName) // RIP the command if user is speedy
+        const properties = await util.getCommandStats(message.guild.id, 'crime', true, false)
+        const uProperties = await util.getUserCommandStats(message.guild.id, message.author.id, 'crime', true, false)
+        if (!util.isCooldown(message, properties, uProperties)) return // if cd is not up, end command
+        const { min, max, minFine, maxFine } = properties
+        const fineAmount = util.intInRange(minFine, maxFine)
+        const cSymbol = await util.getCurrencySymbol(message.guild.id, false)
+        if (!util.isSuccess(properties)) {
+            message.channel.send({
+                embed: util.embedify(
+                    'RED',
+                    message.author.tag,
+                    message.author.displayAvatarURL(),
+                    `You were caught commiting a crime and fined ${cSymbol}${fineAmount.toLocaleString()}`
+                )
+            })
+            await util.changeBal(message.guild.id, message.author.id, fineAmount * -1, false) // subtract fine from bal
+            return await util.setUserCommandStats(message.guild.id, message.author.id, 'crime', { timestamp: util.now() }, false) // if not successful, still reset the timestamp before ending the command
         }
-
-        // reset the timestamp when used
-        util.setUserCommandStats(guild.id, author.id, 'crime', { timestamp: usedWhen })
-
-        if ((Math.random() * 100) < properties.chance) {
-            const fineAmount = util.intInRange(properties.minFine, properties.maxFine)
-            util.changeBal(guild.id, author.id, (fineAmount * -1))
-            return util.errorEmbed(message, `You were caught and fined ${currencySymbol}${fineAmount}`, this.memberName)
-        }
-
-        const min = properties.min; const max = properties.max
         const amount = util.intInRange(min, max)
-
-        util.changeBal(message.guild.id, message.author.id, amount)
-        util.successEmbed(message, `You committed a crime and earned ${currencySymbol}${amount}!`)
+        message.channel.send({
+            embed: util.embedify(
+                'GREEN',
+                message.author.tag,
+                message.author.displayAvatarURL(),
+                `You commited a crime and earned ${cSymbol}${amount.toLocaleString()}!`
+            )
+        })
+        await util.setUserCommandStats(message.guild.id, message.author.id, 'crime', { timestamp: util.now() }, false) // reset timestamp
+        await util.changeBal(message.guild.id, message.author.id, amount) // +close mongo connection
     }
 }
