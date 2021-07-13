495ef35dd7717c35bc21ac2547db62f3a240609c
Adrastopoulos <gabriel.hall@cox.net>
2021-07-13 13:20:41 -0700

    Enable user query

--- commands/economy/market/inventory.js
+++ commands/economy/market/inventory.js
@@ -2,6 +2,7 @@ const { Command } = require('discord.js-commando')
 const mongo = require('../../../features/mongo')
 const util = require('../../../features/util')
 const inventorySchema = require('../../../features/schemas/inventory-sch')
+const { oneLine } = require('common-tags')
 
 module.exports = class InventoryCommand extends Command {
     constructor(client) {
@@ -10,22 +11,48 @@ module.exports = class InventoryCommand extends Command {
             group: 'market',
             memberName: 'inventory',
             guildOnly: true, 
-            description: 'View your inventory.'
+            description: 'View inventory.',
+            details: oneLine`View your inventory or someone else's inventory by specifying the user. 
+                            This can be done with @mention, id, or simply typing part of their name.`,
+            format: '[@mention | id | name]',
+            examples: [
+                'inventory @Adrastopoulos#2753',
+                'inventory adrast'
+            ],
+            args: [
+                {
+                    key: 'user',
+                    prompt: 'Please @mention, name, or provide the id of a user.',
+                    type: 'string',
+                    default: ''
+                }
+            ]
         })
     }
 
-    async run(message) {
+    async run(message, { user }) {
         const { author, guild } = message
+        let id
+        if(user) {
+            id = await util.getUserID(message, user)
+            if(id === 'noMemberFound') {
+                return
+            }
+        } else {
+            id = author.id
+        }
+
+        user = guild.members.cache.get(id).user
         const currencySymbol = await util.getCurrencySymbol(guild.id)
         let inventoryEmbed = util.embedify(
             'BLURPLE',
-            author.tag, 
-            author.displayAvatarURL(),
+            user.tag, 
+            user.displayAvatarURL(),
         ) 
         await mongo().then(async (mongoose) => {
             try {
                 const inventory = await inventorySchema.findOne({
-                    userID: author.id, 
+                    userID: user.id, 
                     guildID: guild.id
                 }) 
 
@@ -40,7 +67,7 @@ module.exports = class InventoryCommand extends Command {
                     }
                 }
 
-                inventoryEmbed.setDescription(`You own \`${i}\` items.`)
+                inventoryEmbed.setDescription(`\`${i}\` Items`)
             } catch(err) {
                 console.log(err)
             } finally {
