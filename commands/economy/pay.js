97b6beba72f411a0d3f257856a87ec61a1ea7a2c
Adrastopoulos <gabriel.hall@cox.net>
2021-07-18 22:59:43 -0700

    Add 'all' param

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -23,9 +23,8 @@ module.exports = class PayCommand extends Command {
               ***REMOVED*** 
                 {
                     key: 'amount',
-                    prompt: 'Please enter an amount more than zero you wish to pay the user.',
-                    type: 'integer',
-                    validate: number => number > 0
+                    prompt: 'Please enter an amount you wish to pay the user.',
+                    type: 'string'
                 }
             ]
         })
@@ -41,23 +40,38 @@ module.exports = class PayCommand extends Command {
         user = guild.members.cache.get(id).user
         const cSymbol = await util.getCurrencySymbol(guild.id, false)
         const { wallet } = await util.getEconInfo(guild.id, author.id)
-        if(wallet < amount) {
-            message.channel.send({ embed: util.embedify(
-                'RED',
-                message.author.username,
-                message.author.displayAvatarURL(),
-                `Insufficient wallet: ${cSymbol}${wallet}`
-            ) })
-            return
+        if(parseInt(amount)) {
+            amount = parseInt(amount)
+            if(wallet < amount) {
+                message.channel.send({ embed: util.embedify(
+                    'RED',
+                    message.author.username,
+                    message.author.displayAvatarURL(),
+                    `Insufficient wallet: ${cSymbol}${wallet}`
+                ) })
+            } else {
+                await util.setEconInfo(guild.id, author.id, -amount, 0, -amount, false)
+                await util.setEconInfo(guild.id, user.id, amount, 0, amount)
+                message.channel.send({ embed: util.embedify(
+                    'GREEN',
+                    message.author.username, 
+                    message.author.displayAvatarURL(),
+                    `Paid ${user.username} ${cSymbol}${amount}`
+                ) })
+            }
+        } else if(amount === 'all') {
+            if(wallet < 1) {
+                message.reply(`Invalid amount! Current wallet: ${cSymbol}${wallet}`)
+            } else {
+                await util.setEconInfo(guild.id, author.id, -wallet, 0, -wallet, false)
+                await util.setEconInfo(guild.id, user.id, wallet, 0, wallet)
+                message.channel.send({ embed: util.embedify(
+                    'GREEN',
+                    message.author.username, 
+                    message.author.displayAvatarURL(),
+                    `Paid ${user.username} ${cSymbol}${wallet}`
+                ) })
+            }
         }
-
-        await util.setEconInfo(guild.id, author.id, -amount, 0, -amount, false)
-        await util.setEconInfo(guild.id, user.id, amount, 0, amount)
-        message.channel.send({ embed: util.embedify(
-            'GREEN',
-            message.author.username, 
-            message.author.displayAvatarURL(),
-            `Paid ${user.username} ${cSymbol}${amount}`
-        ) })
     }
 }
