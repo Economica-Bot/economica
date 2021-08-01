c22ef92f729dfa15b59905186e75b24b23a79656
Adrastopoulos <gabriel.hall@cox.net>
2021-07-31 23:11:09 -0700

    Add command subfolders, permission handling, and alias handling

--- src/index.js
+++ src/index.js
@@ -27,17 +27,12 @@ global.mongo = mongo
 client.on('ready', async () => {
     console.log(`${client.user.tag} Ready`)
 
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-    for(const commandFile of commandFiles) {
-        const command = require(`./commands/${commandFile}`)
-        client.api.applications(process.env.APPLICATION_ID).guilds(process.env.GUILD_ID).commands.post({data: {
-            name: command.name,
-            description: command.description,
-            options: command.options
-        }})
-
-        client.commands.set(command.name, command)
-        console.log(`${command.name} command registered`)
+    const commandDirectories = fs.readdirSync('./commands')
+    for(const commandDirectory of commandDirectories) {
+        const commandFiles = fs.readdirSync(`./commands/${commandDirectory}/`).filter(file => file.endsWith('js'))
+        for(const commandFile of commandFiles) {
+            registerCommandFile(commandDirectory, commandFile)
+        }
     }
 
     await mongo().then(async () => {
@@ -50,16 +45,77 @@ client.on('ready', async () => {
 
 client.ws.on('INTERACTION_CREATE', async interaction => {
     try {
-        client.commands.get(interaction.data.name).run(interaction)
+        const command = client.commands.get(interaction.data.name) 
+                    || client.commands.find(cmd => cmd.aliases.some(u => u === interaction.data.name))
+        const guild = await client.guilds.cache.get(interaction.guild_id)
+        const author = await guild.members.cache.get(interaction.member.user.id)
+        const args = interaction.data.options
+        const missingPermissions = command.permissions ? hasPermissions(author, command.permissions) : null
+        if(missingPermissions) {
+            const embed = util.embedify(
+                'RED', 
+                author.user.username, 
+                author.user.displayAvatarURL(), 
+                `You are missing the ${missingPermissions.substring(0, missingPermissions.length - 2)} permission(s) to run this command.`
+            )
+            
+            say(interaction, null, embed, 64)
+            return
+        }
+        command.run(interaction, guild, author, args)
     } catch (err) {
         console.error(err)
-        client.api.interactions(interaction.id, interation.token).callback.post({ data: {
+        client.api.interactions(interaction.id, interaction.token).callback.post({ data: {
             type: 4, 
             data: {
-                content: 'Error'
+                content: `${err.message}`
             }
         }})
     }   
 })
 
 client.login(process.env.ECON_TOKEN)
+
+const registerCommandFile = (commandDirectory, commandFile) => {
+    const command = require(`./commands/${commandDirectory}/${commandFile}`)
+    client.api.applications(process.env.APPLICATION_ID).guilds(process.env.GUILD_ID).commands.post({data: {
+        name: command.name,
+        description: command.description,
+        options: command.options
+    }})
+
+    if(command.aliases) {
+        for(const alias of command.aliases) {
+            client.api.applications(process.env.APPLICATION_ID).guilds(process.env.GUILD_ID).commands.post({data: {
+                name: alias,
+                description: command.description,
+                options: command.options
+            }})
+        }
+    }
+
+    client.commands.set(command.name, command)
+    console.log(`${command.name} command registered`)
+}
+
+const hasPermissions = (author, permissions) => {
+    let missingPermissions = ''
+    for(const permission of permissions) {
+        if(!author.permissions.has(permission)) {
+            missingPermissions += `\`${permission}\`, `     
+        }
+    }
+
+    return missingPermissions.length ? missingPermissions : null
+}
+
+const say = async (interaction, content = null, embed = null, flags = null) => {
+    await client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+        type: 4,
+        data: {
+            content,
+            embeds: [ embed ], 
+            flags
+      ***REMOVED***
+    }})
+}
