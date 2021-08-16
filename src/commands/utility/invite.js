cd7bcf01691c58ef55278a5a537a3e5e4e5a30cd
Adrastopoulos <gabriel.hall@cox.net>
2021-08-16 16:29:58 -0700

    feat: invite command

---
+++ src/commands/utility/invite.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'invite',
+  group: 'utility',
+  description: 'Gets the invite link for Economica.',
+  options: null,
+  async run(interaction) {
+    interaction.reply({
+      embeds: [
+        util.embedify(
+          'GOLD',
+          client.user.username,
+          client.user.displayAvatarURL(),
+          `Invite link: __[Click Here](${process.env.INVITE_LINK} 'Invite Economica')__`
+        ),
+      ],
+    });
+***REMOVED***
+};
