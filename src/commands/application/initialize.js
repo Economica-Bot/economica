9788ee62de1f10ba614f0bc4b177cf1539fabb34
Adrastopoulos <gabriel.hall@cox.net>
2021-08-26 10:14:02 -0700

    fix: make initialize owner only cmd

--- src/commands/application/initialize.js
+++ src/commands/application/initialize.js
@@ -3,6 +3,7 @@ module.exports = {
   group: 'application',
   description: 'Initialize the database.',
   options: null,
+  ownerOnly: true, 
   async run(interaction, guild, author, options) {
     const guildSettings = await util.initGuildSettings(guild);
     await interaction.reply(`Init \`\`\`${guildSettings.toString()}\`\`\``);
