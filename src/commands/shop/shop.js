0b80047e992aa605d056f48efa3653991eb0f042
Adrastopoulos <gabriel.hall@cox.net>
2021-10-31 22:03:51 -0700

    fix: paginate

--- src/commands/shop/shop.js
+++ src/commands/shop/shop.js
@@ -8,35 +8,52 @@ module.exports = {
   global: true,
   options: null,
   async run(interaction) {
-    let embed = new Discord.MessageEmbed();
-    embed
-      .setAuthor(`${interaction.guild.name} Shop`, interaction.guild.iconURL())
-      .setColor('BLUE');
+    await interaction.deferReply();
+
     const items = await util.getShopItems(interaction.guild.id);
     if (items.length === 0) {
-      embed.setDescription(
+      interaction.reply(
         `There are currently \`0\` items in the shop.\nAsk your Economy Manager to add some!`
       );
+      return;
     } else {
+      const embeds = [];
+      let entries = 15;
+      let pageCount = Math.ceil(items.length / entries);
+      const shopEntries = [];
       const currencySymbol = await util.getCurrencySymbol(interaction.guild.id);
-      let count = 0;
       items.forEach((item) => {
         if (item.active) {
-          count++;
-          embed.addField(
-            `${currencySymbol}${
-              item.price > 0 ? util.num(item.price) : 'Free'
-            } - ${util.cut(item.name)}`,
-            util.cut(item.description, 200),
-            item.description?.length > 100 ? false : true
-          );
+          shopEntries.push(`${currencySymbol}**${
+            item.price > 0 ? util.num(item.price) : 'Free'
+          } - __*${util.cut(item.name)}*__**\n
+            ${util.cut(item.description, 200)}\n\n`);
         }
       });
-      embed.setDescription(
-        `There are currently \`${count}\` items in the shop. Use the \`item view\` command to view detailed item stats, use the \`item buy\` command to purchase an item!`
-      );
-    }
 
-    await interaction.reply({ embeds: [embed] });
+      let k = 0;
+      for (let i = 0; i < pageCount; i++) {
+        description = '';
+        for (let j = 0; j < entries; j++) {
+          if (shopEntries[k]) {
+            description += shopEntries[k++];
+          }
+        }
+        embeds.push(
+          new Discord.MessageEmbed()
+            .setAuthor(
+              `${interaction.guild.name} Shop`,
+              interaction.guild.iconURL()
+            )
+            .setColor('BLUE')
+            .setDescription(description)
+            .setFooter(
+              `There are currently \`${k}\` items in the shop. Use the \`item view\` command to view detailed item stats, use the \`item buy\` command to purchase an item!`
+            )
+        );
+      }
+
+      await util.paginate(interaction, embeds);
+    }
 ***REMOVED***
 };
