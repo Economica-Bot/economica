373b02e2af870f004d4d743efe5d7f309dde4d48
Adrastopoulos <gabriel.hall@cox.net>
2021-12-27 19:41:21 -0800

    style: cleanup

--- src/commands/utility/permissions.ts
+++ src/commands/utility/permissions.ts
@@ -1,52 +1,76 @@
 import { CommandInteraction } from 'discord.js';
-import { PermissionRole } from '../../structures/CommandOptions';
 import EconomicaClient from '../../structures/EconomicaClient';
 import EconomicaCommand from '../../structures/EconomicaCommand';
 import { EconomicaSlashCommandBuilder } from '../../structures/EconomicaSlashCommandBuilder';
 import { embedify } from '../../util';
 
-export default class extends EconomicaCommand {
+export default class PermissionsCommand extends EconomicaCommand {
 	data = new EconomicaSlashCommandBuilder()
 		.setName('permission')
-		.setDescription('Permission test command.')
-		.setClientPermissions(['ADD_REACTIONS', 'SEND_MESSAGES'])
-		.setUserPermissions(['ADMINISTRATOR', 'VIEW_AUDIT_LOG'])
-		.setRoles([new PermissionRole('EXAMPLE_ROLE', true)])
-		.addEconomicaSubcommandGroup((subcommandgroup) =>
-			subcommandgroup
-				.setName('subcommandgroup')
-				.setDescription('Example subcommand group.')
-				.setClientPermissions(['CHANGE_NICKNAME'])
-				.setUserPermissions(['CREATE_INSTANT_INVITE', 'ATTACH_FILES', 'CONNECT'])
-				.addEconomicaSubcommand((subcommand) => subcommand.setName('subcommand1').setDescription('Example subcommand without overrides.'))
-				.addEconomicaSubcommand((subcommand) =>
-					subcommand
-						.setName('subcommand2')
-						.setDescription('Example subcommand with overrides.')
-						.setClientPermissions(['DEAFEN_MEMBERS'])
-						.setUserPermissions(['KICK_MEMBERS'])
-						.setRoles([new PermissionRole('SUBCOMMAND_ONLY_ROLE', true)])
-				)
-		)
-		.addEconomicaSubcommand((subcommand) => subcommand.setName('subcommand3').setDescription('Top level subcommand'));
+		.setDescription('See the permissions of a command.')
+		.setFormat('<command>')
+		.addStringOption((option) =>
+			option
+				.setName('command')
+				.setDescription('Specify a command.')
+				.setRequired(true)
+		);
 
-	execute = async (client: EconomicaClient, interaction: CommandInteraction) => {
-		const data = this.data as EconomicaSlashCommandBuilder;
+	execute = async (
+		client: EconomicaClient,
+		interaction: CommandInteraction
+	) => {
+		const commandInput = interaction.options.getString('command');
+		const command = client.commands.get(commandInput);
+		if (!command) {
+			return interaction.reply('Could not find that command.');
+		}
+
+		const data = command.data as EconomicaSlashCommandBuilder;
+
+		let description = `**${
+			data.name
+		} Command Permissions**\n__User Permissions__:\n\`${
+			data.userPermissions ?? '`None`'
+		}\`\n__Client Permissions:__\n\`${
+			data.clientPermissions ?? '`None`'
+		}\`\n__Roles:__\n\`${
+			data.roles ?? '`None`'
+		}\`\n\n`;
+
+		if (data.getSubcommandGroup(interaction)) {
+			description += `**${
+				data.getSubcommandGroup(interaction).name
+			}Subcommand Group Permissions**\n__User Permissions:__\n\`${
+				data.getSubcommandGroup(interaction).userPermissions ?? '`None`'
+			}\`\n__Client Permissions:__\n\`${
+				data.getSubcommandGroup(interaction).clientPermissions ?? '`None`' 
+			}\`\n__Roles:__\n\`${
+				data.getSubcommandGroup(interaction).roles ?? '`None`'
+			}\`\n\n`;
+		}
+
+		if (data.getSubcommand(interaction)) {
+			`**${
+				data.getSubcommand(interaction).name
+			} Subcommand Permissions**\n__User Permissions:__\n\`${
+				data.getSubcommand(interaction).userPermissions ?? '`None`'
+			}\`\n__Client Permissions:__\n\`${
+				data.getSubcommand(interaction).clientPermissions ?? '`None`'
+			}\`\n__Roles:__\n\`${
+				data.getSubcommand(interaction).roles ?? '`None`'
+			}\``;
+		}
+
+		const name = data.name + data.getSubcommandGroup(interaction) ?? '' + data.getSubcommand(interaction) ?? ''
+
 		interaction.reply({
 			embeds: [
 				embedify(
 					'BLUE',
-					'Permission Hierarchy for Permissions Command',
+					`Permission Hierarchy for ${command.data.name} ${name}`,
 					null,
-					`**${
-						data.name
-					} Command Permissions**\n__User Permissions__:\n\`${data.getUserPermissions()}\`\n__Client Permissions:__\n\`${data.getClientPermissions()}\`\n__Roles:__\n\`${data.getRoles()}\`\n\n**${
-						data.getSubcommandGroup(interaction).name
-					} Subcommand Group Permissions**\n__User Permissions:__\n\`${data.getSubcommandGroup(interaction).userPermissions}\`\n__Client Permissions:__\n\`${
-						data.getSubcommandGroup(interaction).clientPermissions
-					}\`\n__Roles:__\n\`${data.getSubcommandGroup(interaction).roles}\`\n\n**${data.getSubcommand(interaction).name} Subcommand Permissions**\n__User Permissions:__\n\`${
-						data.getSubcommand(interaction).userPermissions
-					}\`\n__Client Permissions:__\n\`${data.getSubcommand(interaction).clientPermissions}\`\n__Roles:__\n\`${data.getSubcommand(interaction).roles}\``
+					description
 				),
 			],
 		});
--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,6 +1,4 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import { PermissionRole } from '../../structures/CommandOptions';
 import EconomicaClient from '../../structures/EconomicaClient';
 import EconomicaCommand from '../../structures/EconomicaCommand';
 import { EconomicaSlashCommandBuilder } from '../../structures/EconomicaSlashCommandBuilder';
@@ -11,33 +9,12 @@ export default class PingCommand extends EconomicaCommand {
 		.setName('ping')
 		.setDescription("Get Economica's latency.")
 		.setGlobal(true)
-		.setGroup('Utility')
-		.setFormat('ping')
-		.setRoles([new PermissionRole('Ping_Role', true)])
-		.setClientPermissions(['SEND_MESSAGES'])
-		.setUserPermissions(['SEND_MESSAGES'])
-		.addEconomicaSubcommandGroup((subcommandgroup) =>
-			subcommandgroup
-				.setName('testsubcommandgroup')
-				.setDescription('Description')
-				.addEconomicaSubcommand((subcommand) =>
-					subcommand
-						.setName('test')
-						.setDescription('This is a test')
-						.addStringOption((option) =>
-							option.setName('test').setDescription('Description')
-						)
-				)
-		);
+		.setGroup('Utility');
 
 	execute = async (
 		client: EconomicaClient,
 		interaction: CommandInteraction
 	) => {
-		const test = new SlashCommandBuilder().addSubcommand((subcommand) =>
-			subcommand.setName('test')
-		);
-
 		await interaction.reply({
 			embeds: [
 				util.embedify(
