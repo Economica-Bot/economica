6aeeb32c6c2d3de118dafa60f818003bf8e8222f
Adrastopoulos <gabriel.hall@cox.net>
2021-12-29 14:28:14 -0800

    refactor: move command checks to util

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,17 +1,12 @@
-import {
-	CommandInteraction,
-	Guild,
-	GuildMember,
-	PermissionResolvable,
-	PermissionString,
-	TextChannel,
-} from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import {
 	EconomicaClient,
 	EconomicaCommand,
-	PermissionRole,
 	EconomicaSlashCommandBuilder,
 } from '../structures/index';
+import { runtimeError } from '../util/util';
+import { commandCheck } from '../util/command';
+
 export const name = 'interactionCreate';
 
 export async function execute(client: EconomicaClient, interaction: CommandInteraction) {
@@ -20,52 +15,20 @@ export async function execute(client: EconomicaClient, interaction: CommandInter
 	}
 
 	const command = client.commands.get(interaction.commandName) as EconomicaCommand;
+	try {
+		if (!command) {
+			throw new Error(`There was an error while executing this command`);
+		}
+	} catch (err) {
+		runtimeError(client, err, interaction);
+	}
+
 	const data = command.data as EconomicaSlashCommandBuilder;
-	permissionCheck(interaction, data);
-
-	await command?.execute(client, interaction);
-}
-
-async function permissionCheck(
-	interaction: CommandInteraction,
-	data: EconomicaSlashCommandBuilder
-) {
-	const member = interaction.member as GuildMember;
-	const guild = interaction.guild as Guild;
-	const clientMember = (await guild.members.fetch(interaction.client.user.id)) as GuildMember;
-	const channel = interaction.channel as TextChannel;
-	const group = data.getSubcommandGroup(interaction);
-	const subcommand = data.getSubcommand(interaction);
-	const userPermissions: PermissionString[] = [];
-	const clientPermissions: PermissionString[] = [];
-	const roles: PermissionRole[] = [];
-	const missingUserPermissions: PermissionString[] = [];
-	const missingClientPermissions: PermissionString[] = [];
-	const missingRoles: PermissionRole[] = [];
-
-	if (data.userPermissions) userPermissions.push(...data.userPermissions);
-	if (data.clientPermissions) clientPermissions.push(...data.clientPermissions);
-	if (data.roles) roles.push(...data.roles);
-	if (group.userPermissions) userPermissions.push(...group.userPermissions);
-	if (group.clientPermissions) clientPermissions.push(...group.clientPermissions);
-	if (group.roles) roles.push(...group.roles);
-	if (subcommand.userPermissions) userPermissions.push(...subcommand.userPermissions);
-	if (subcommand.clientPermissions) clientPermissions.push(...subcommand.clientPermissions);
-	if (subcommand.roles) roles.push(...subcommand.roles);
-
-	for (const permission of userPermissions)
-		if (!member.permissionsIn(channel).has(permission)) missingUserPermissions.push(permission);
-	for (const permission of userPermissions)
-		if (!clientMember.permissionsIn(channel).has(permission))
-			missingClientPermissions.push(permission);
-	for (const role of roles) {
-		const guildRole = (await guild.roles.fetch()).find(
-			(r) => r.name.toLowerCase() === role.name.toLowerCase()
-		);
-		if (!guildRole)
-			missingRoles.push(
-				new PermissionRole(`Please create an \`${role.name}\` role. Case insensitive.`, true)
-			);
-		else if (role.required && !member.roles.cache.has(guildRole.id)) missingRoles.push(role);
+	try {
+		if (await commandCheck(interaction, data)) {
+			await command?.execute(client, interaction);
+		}
+	} catch (err) {
+		runtimeError(client, err, interaction);
 	}
 }
---
+++ src/util/command.ts
@@ -0,0 +1,80 @@
+import { CommandInteraction, Guild, GuildMember, PermissionString, TextChannel } from 'discord.js';
+import { EconomicaSlashCommandBuilder, PermissionRole } from '../structures';
+
+export async function commandCheck(
+	interaction: CommandInteraction,
+	data: EconomicaSlashCommandBuilder
+): Promise<boolean> {
+	if (data.devOnly && !process.env.OWNERID.includes(interaction.user.id)) {
+		interaction.reply({ content: 'This command is dev only.', ephemeral: true });
+		return false;
+	}
+
+	if (!data.global && !interaction.guild) {
+		interaction.reply({ content: 'This command is server only.', ephemeral: true });
+		return false;
+	}
+
+	if (interaction.member && !(await permissionCheck(interaction, data))) {
+		interaction.reply({ content: 'Insufficient permissions.', ephemeral: true });
+		return false;
+	}
+
+	return true;
+}
+
+const permissionCheck = async (
+	interaction: CommandInteraction,
+	data: EconomicaSlashCommandBuilder
+): Promise<boolean> => {
+	const member = interaction.member as GuildMember;
+	const guild = interaction.guild as Guild;
+	const clientMember = (await guild.members.fetch(interaction.client.user.id)) as GuildMember;
+	const channel = interaction.channel as TextChannel;
+	const group = data.getSubcommandGroup(interaction);
+	const subcommand = data.getSubcommand(interaction);
+	const userPermissions: PermissionString[] = [];
+	const clientPermissions: PermissionString[] = [];
+	const roles: PermissionRole[] = [];
+	const missingUserPermissions: PermissionString[] = [];
+	const missingClientPermissions: PermissionString[] = [];
+	const missingRoles: PermissionRole[] = [];
+
+	if (
+		process.env.OWNERID.includes(member.id) ||
+		interaction.guild.ownerId === member.id ||
+		member.permissions.has('MANAGE_GUILD')
+	) {
+		return true;
+	}
+
+	if (data.userPermissions) userPermissions.push(...data.userPermissions);
+	if (data.clientPermissions) clientPermissions.push(...data.clientPermissions);
+	if (data.roles) roles.push(...data.roles);
+	if (group?.userPermissions) userPermissions.push(...group.userPermissions);
+	if (group?.clientPermissions) clientPermissions.push(...group.clientPermissions);
+	if (group?.roles) roles.push(...group.roles);
+	if (subcommand?.userPermissions) userPermissions.push(...subcommand.userPermissions);
+	if (subcommand?.clientPermissions) clientPermissions.push(...subcommand.clientPermissions);
+	if (subcommand?.roles) roles.push(...subcommand.roles);
+
+	for (const permission of userPermissions)
+		if (!member.permissionsIn(channel).has(permission)) missingUserPermissions.push(permission);
+	for (const permission of userPermissions)
+		if (!clientMember.permissionsIn(channel).has(permission))
+			missingClientPermissions.push(permission);
+	for (const role of roles) {
+		const guildRole = (await guild.roles.fetch()).find(
+			(r) => r.name.toLowerCase() === role.name.toLowerCase()
+		);
+		if (!guildRole)
+			missingRoles.push(
+				new PermissionRole(`Please create an \`${role.name}\` role. Case insensitive.`, true)
+			);
+		else if (role.required && !member.roles.cache.has(guildRole.id)) missingRoles.push(role);
+	}
+
+	if (missingClientPermissions.length || missingUserPermissions.length || missingRoles.length)
+		return false;
+	return true;
+};
