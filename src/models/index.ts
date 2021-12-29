eaf3b0181943da5cfaff04842f3a70028433b157
Adrastopoulos <gabriel.hall@cox.net>
2021-12-28 22:54:10 -0800

    refactor: bundle model exports

---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+export { GuildModel } from './guilds';
+export { InfractionModel } from './infractions';
+export { LoanModel } from './loans';
+export { MarketModel } from './markets';
+export { MemberModel } from './members';
+export { ShopModel } from './shops';
+export { TransactionModel } from './transactions';
