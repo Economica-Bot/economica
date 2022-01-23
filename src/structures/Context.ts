a34ecee9fbe99fd46be89f99538b8f445472cc22
Adrastopoulos <gabriel.hall@cox.net>
2022-01-23 13:59:42 -0800

    fix: Document<Guild> in context

--- src/structures/Context.ts
+++ src/structures/Context.ts
@@ -1,17 +1,17 @@
 import { CommandInteraction } from 'discord.js';
-import { Document, Model } from 'mongoose';
+import { Document } from 'mongoose';
 import { EconomicaClient } from '.';
 import { Guild } from '../models/guilds';
 
 export class Context {
 	public client: EconomicaClient;
 	public interaction: CommandInteraction;
-	public guildDocument: Guild;
+	public guildDocument: Guild & Document<Guild>;
 
 	public constructor(
 		client: EconomicaClient,
 		interaction: CommandInteraction,
-		guildDocument: Guild
+		guildDocument: Guild & Document<Guild>
 	) {
 		this.client = client;
 		this.interaction = interaction;
