b65a4292dfece040318253bc86e03edf350bd8db
Adrastopoulos <gabriel.hall@cox.net>
2022-03-17 19:10:25 -0700

    feat: log discord errors

--- src/events/error.ts
+++ src/events/error.ts
@@ -5,6 +5,7 @@ import { Economica, Event } from '../structures/index.js';
 export default class implements Event {
 	public event = 'error' as const;
 	public async execute(client: Economica, error: Error): Promise<void> {
+		client.log.error(error);
 		client.webhooks.forEach(async (webhook) => {
 			const description = `\`\`\`ts\n${error}\`\`\``;
 			const embed = new EmbedBuilder().setAuthor({ name: 'Discord Error' }).setDescription(description);
