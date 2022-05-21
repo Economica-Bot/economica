02f7ad9b282ba2ccaba1033e00a9a4d27930578a
Adrastopoulos <gabriel.hall@cox.net>
2022-05-20 19:54:59 -0700

    feat: choosinator + paginator

--- src/structures/Context.ts
+++ src/structures/Context.ts
@@ -1,8 +1,23 @@
 /* eslint-disable max-classes-per-file */
-import { ChatInputCommandInteraction, EmbedBuilder, Util } from 'discord.js';
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonInteraction,
+	ButtonStyle,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	EmojiResolvable,
+	InteractionReplyOptions,
+	Message,
+	MessageComponentInteraction,
+	SelectMenuBuilder,
+	SelectMenuInteraction,
+	Util,
+} from 'discord.js';
+import _ from 'lodash';
 
 import { Guild, Member, User } from '../entities/index.js';
-import { EmbedColors, Footer, ReplyString } from '../typings/index.js';
+import { EmbedColors, Footer, PAGINATION_LIMIT, ReplyString, Emojis } from '../typings/index.js';
 import { Economica, EconomicaSlashCommandBuilder } from './index.js';
 
 export class ContextEmbed extends EmbedBuilder {
@@ -66,4 +81,72 @@ export class Context {
 		else embed.setFooter(footer);
 		return embed;
 	}
+
+	public async selectinator<T extends ValidInteraction>(interaction: T, base: ContextEmbed, data: ChooseData[]): Promise<{
+		interaction: SelectMenuInteraction<'cached'>,
+		res: ChooseData
+	}> {
+		const pageCount = Math.ceil(data.length / PAGINATION_LIMIT) || 1;
+		const pages: InteractionReplyOptions[] = [];
+		const numbers: Record<number, `${Emojis}`> = {
+			1: '<:icon_1:974903674868498482>',
+			2: '<:icon_2:974903675891879997>',
+			3: '<:icon_3:974903677087281222>',
+			4: '<:icon_4:974903678920196157>',
+			5: '<:icon_5:974903678018404372>',
+		};
+		for (let i = 0; i < pageCount; i++) {
+			const embed = new EmbedBuilder(base.data);
+			const selectMenu = new SelectMenuBuilder().setCustomId('select');
+			const dataSubset = data.slice(i * PAGINATION_LIMIT, i * PAGINATION_LIMIT + PAGINATION_LIMIT);
+			embed.addFields(dataSubset.map((d, j) => ({ name: `${numbers[j + 1]} ${d.emoji || ''} ${d.name}`, value: d.description })));
+			selectMenu.addOptions(dataSubset.map((d, j) => ({ emoji: { id: Util.parseEmoji(numbers[j + 1]).id }, label: `${d.clean}`, value: d.value })));
+			const row = new ActionRowBuilder<SelectMenuBuilder>().setComponents([selectMenu]);
+			const page: InteractionReplyOptions = { embeds: [embed], components: selectMenu.options.length ? [row] : [] };
+			pages.push(page);
+		}
+
+		const selectMenuInteraction = await this.paginator<T>({ pages, interaction });
+		const res = data.find((d) => d.value === selectMenuInteraction.values['0']);
+		return { interaction: selectMenuInteraction, res };
+	}
+
+	public async paginator<T extends ValidInteraction>({ pages, interaction, index = 0 }: {
+		pages: InteractionReplyOptions[],
+		interaction?: T,
+		index?: number
+	}) {
+		const component = new ActionRowBuilder<ButtonBuilder>()
+			.setComponents([
+				new ButtonBuilder()
+					.setCustomId('previous')
+					.setLabel('Previous Page')
+					.setStyle(ButtonStyle.Secondary)
+					.setDisabled(index === 0),
+				new ButtonBuilder()
+					.setCustomId('next')
+					.setLabel('Next Page')
+					.setStyle(ButtonStyle.Primary)
+					.setDisabled(index + 1 === pages.length),
+			]);
+
+		const reply = _.cloneDeep(pages[index]);
+		reply.components.push(component);
+		let message: Message<true>;
+		if (interaction.isChatInputCommand()) message = await interaction.reply({ ...reply, fetchReply: true });
+		else message = await interaction.update({ ...reply, fetchReply: true } as any);
+		const res = await message.awaitMessageComponent();
+		if (res.isSelectMenu()) return res as any;
+		return this.paginator({ pages, interaction: res, index: res.customId === 'next' ? index + 1 : index - 1 });
+	}
 }
+
+type ValidInteraction = ChatInputCommandInteraction<'cached'> | SelectMenuInteraction<'cached'> | ButtonInteraction<'cached'> | MessageComponentInteraction<'cached'>;
+
+export type ChooseData = {
+	name: string,
+	clean: string,
+	description: string,
+	value: string,
+	emoji?: EmojiResolvable
+};
