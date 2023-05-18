11c021f5b2ac9d85ad02581f830d0bd52c0558c9
Gabriel Hall <adrastopoulos@gmail.com>
2023-05-18 15:44:21 -0700

    update developer card

--- apps/dashboard/src/components/misc/DeveloperCard.tsx
+++ apps/dashboard/src/components/misc/DeveloperCard.tsx
@@ -10,16 +10,19 @@ type Props = {
 
 export const DeveloperCard: FC<Props> = ({ src, name, link }) => (
 	<div className="flex flex-col items-center py-5">
-		<div className="relative h-32 w-32 transition duration-500 hover:scale-110">
+		<div className="relative h-32 w-32 transition duration-500 hover:scale-105">
 			<Image
 				src={src}
-				alt={src}
+				alt={name}
 				className="rounded-full drop-shadow-md"
 				width={128}
 				height={128}
 			/>
 		</div>
-		<Link href={link} className="text-underline mt-3 text-lg font-thin">
+		<Link
+			href={link}
+			className="relative mt-3 text-lg font-thin before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-left before:scale-x-0 before:bg-base-content before:transition-all hover:before:scale-x-100"
+		>
 			{name}
 		</Link>
 	</div>
