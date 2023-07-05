c4e9123599cba810ffdbb91672cbcdd680f8311b
Gabriel Hall <gabriel.hall@cox.net>
2023-07-04 20:55:21 -0700

    generate bot invitation

--- apps/dashboard/src/pages/api/invite.ts
+++ apps/dashboard/src/pages/api/invite.ts
@@ -1,6 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { env } from '../../env.mjs';
+import {
+	OAuth2Scopes,
+	PermissionFlagsBits,
+	RouteBases,
+	Routes
+} from 'discord-api-types/v10';
+import { makeURLSearchParams } from '@discordjs/rest';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.redirect(env.INVITE_BOT_URL);
+	const query = makeURLSearchParams({
+		client_id: env.DISCORD_CLIENT_ID,
+		scope: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands].join(' ')
+	});
+
+	const guildId = req.query.guildId;
+	if (guildId && typeof guildId === 'string') query.set('guild_id', guildId);
+
+	const permissions = [
+		PermissionFlagsBits.ViewChannel,
+		PermissionFlagsBits.SendMessages,
+		PermissionFlagsBits.EmbedLinks,
+		PermissionFlagsBits.BanMembers,
+		PermissionFlagsBits.ModerateMembers,
+		PermissionFlagsBits.ManageMessages,
+		PermissionFlagsBits.UseExternalEmojis
+	];
+	query.set('permissions', permissions.reduce((a, b) => a | b, 0n).toString());
+
+	res.redirect(`${RouteBases.api}${Routes.oauth2Authorization()}?${query}`);
 }
